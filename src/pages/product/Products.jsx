00d9dc696f966849f313ed89fb4f3d0638c14a78
mumtaz <mumtazkhang7868@gmail.com>
2023-08-30 14:03:03 +0500

    fetch fake api done

--- src/pages/product/Products.jsx
+++ src/pages/product/Products.jsx
@@ -1,8 +1,19 @@
 import AddProduct from "./AddProduct";
 import ProductData from "./ProductData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 const Products = () => {
   const [showModal, setShowModal] = useState();
+  const [data, setData] = useState([]);
+  const getProduct = async () => {
+    const result = await fetch("https://fakestoreapi.com/products");
+    const actualData = await result.json();
+    console.log(actualData, "title");
+    setData(actualData);
+  };
+  useEffect(() => {
+    getProduct();
+  }, []);
+
   return (
     <>
       {showModal ? (
@@ -10,17 +21,19 @@ const Products = () => {
       ) : null}
 
       <div className="mx-4 md:mx-8 xl:mx-10 flex flex-wrap gap-6 mt-6 mb-5 justify-between">
-        {ProductData.map((item, index) => (
+        {data.map((item) => (
           <div
             className="w-[279px] rounded-lg bg-white shadow-lg flex flex-col max-xl:flex-grow"
-            key={index}
+            key={item.id}
           >
-            <img className="h-[300px] rounded-lg" src={item.img} alt="" />
-            <p className="mt-3 text-lg px-2">{item.title}</p>
-            <p className="text-lg mt-2 text-[#F59187] px-2">
+            <img className="h-[300px] rounded-lg" src={item.image} alt="" />
+            <p className="mt-4 text-lg px-4 truncate">{item.title}</p>
+            <p className="text-lg mt-2 text-[#F59187] px-4">
               Rs.<b>{item.price}</b>
             </p>
-            <p className="text-sm my-1 mx-2">{item.description}</p>
+            <p className="text-sm mx-4 mt-2 mb-4 line-clamp-3 overflow-ellipsis">
+              {item.description}
+            </p>
           </div>
         ))}
         <div className="flex w-full justify-end relative">
